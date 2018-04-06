0a075ffd3b5019208e58e91cd17167958ae6c2ad
xylitogum <xylitogum@gmail.com>
2018-04-06 17:30:22 -0400

    Added ConditionalHide Property Editor Scripts

---
+++ Assets/X-Utils/Properties/Editor/ConditionalHideDrawer.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(ConditionalHideAttribute))]
+class ConditionalHideDrawer : PropertyDrawer
+{
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        //get the attribute data
+        ConditionalHideAttribute condHAtt = (ConditionalHideAttribute)attribute;
+        //check if the propery we want to draw should be enabled
+        bool enabled = GetConditionalHideAttributeResult(condHAtt, property);
+
+        //Enable/disable the property
+        bool wasEnabled = GUI.enabled;
+        GUI.enabled = enabled;
+
+        //Check if we should draw the property
+        if (!condHAtt.HideInInspector || enabled)
+        {
+            EditorGUI.PropertyField(position, property, label, true);
+        }
+
+        //Ensure that the next property that is being drawn uses the correct settings
+        GUI.enabled = wasEnabled;
+    }
+
+
+    private bool GetConditionalHideAttributeResult(ConditionalHideAttribute condHAtt, SerializedProperty property)
+    {
+        bool enabled = true;
+        //Look for the sourcefield within the object that the property belongs to
+        string propertyPath = property.propertyPath; //returns the property path of the property we want to apply the attribute to
+        string conditionPath = propertyPath.Replace(property.name, condHAtt.ConditionalSourceField); //changes the path to the conditionalsource property path
+        SerializedProperty sourcePropertyValue = property.serializedObject.FindProperty(conditionPath);
+
+        if (sourcePropertyValue != null)
+        {
+            enabled = sourcePropertyValue.boolValue;
+        }
+        else
+        {
+            Debug.LogWarning("Attempting to use a ConditionalHideAttribute but no matching SourcePropertyValue found in object: " + condHAtt.ConditionalSourceField);
+        }
+
+        return enabled;
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        ConditionalHideAttribute condHAtt = (ConditionalHideAttribute)attribute;
+        bool enabled = GetConditionalHideAttributeResult(condHAtt, property);
+
+        if (!condHAtt.HideInInspector || enabled)
+        {
+            return EditorGUI.GetPropertyHeight(property, label);
+        }
+        else
+        {
+            //The property is not being drawn
+            //We want to undo the spacing added before and after the property
+            return -EditorGUIUtility.standardVerticalSpacing;
+        }
+    }
+
+
+}
---
+++ Assets/X-Utils/Properties/Editor/EnumHideDrawer.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(EnumHideAttribute))]
+class EnumHideDrawer : PropertyDrawer
+{
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        //get the attribute data
+        EnumHideAttribute enumHAtt = (EnumHideAttribute)attribute;
+        //check if the propery we want to draw should be enabled
+        bool enabled = GetEnumHideAttributeResult(enumHAtt, property);
+
+        //Enable/disable the property
+        bool wasEnabled = GUI.enabled;
+        GUI.enabled = enabled;
+
+
+        //Check if we should draw the property
+        if (!enumHAtt.HideInInspector || enabled)
+        {
+            EditorGUI.PropertyField(position, property, label, true);
+        }
+
+        //Ensure that the next property that is being drawn uses the correct settings
+        GUI.enabled = wasEnabled;
+    }
+
+
+    private bool GetEnumHideAttributeResult(EnumHideAttribute enumHAtt, SerializedProperty property)
+    {
+        bool enabled = false;
+        //Look for the sourcefield within the object that the property belongs to
+        string propertyPath = property.propertyPath; //returns the property path of the property we want to apply the attribute to
+        string enumPath = propertyPath.Replace(property.name, enumHAtt.EnumSourceField); //changes the path to the enumsource property path
+        int sourcePropertyValue = property.serializedObject.FindProperty(enumPath).enumValueIndex;
+        
+
+        if (sourcePropertyValue == enumHAtt.EnumOption)
+        {
+            enabled = true;
+        }
+        else
+        {
+            //Debug.LogWarning("Attempting to use a ConditionalHideAttribute but no matching SourcePropertyValue found in object: " + enumHAtt.EnumSourceField);
+        }
+
+        return enabled;
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        EnumHideAttribute enumHAtt = (EnumHideAttribute)attribute;
+        bool enabled = GetEnumHideAttributeResult(enumHAtt, property);
+
+        if (!enumHAtt.HideInInspector || enabled)
+        {
+            return EditorGUI.GetPropertyHeight(property, label);
+        }
+        else
+        {
+            //The property is not being drawn
+            //We want to undo the spacing added before and after the property
+            return -EditorGUIUtility.standardVerticalSpacing;
+        }
+    }
+
+
+}
