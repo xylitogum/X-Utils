54d5968162e9fca5c0ef8723c08164e93c98186c
xylitogum <xylitogum@gmail.com>
2018-04-06 17:30:48 -0400

    Added ConditionalHide Property Scripts

---
+++ Assets/X-Utils/Properties/EnumHideAttribute.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property |
+    AttributeTargets.Class | AttributeTargets.Struct, Inherited = true)]
+public class EnumHideAttribute : PropertyAttribute
+{
+    //The name of the bool field that will be in control
+    public string EnumSourceField = "";
+
+    //The name of the bool field that will be in control
+    public int EnumOption = 0;
+
+
+    //TRUE = Hide in inspector / FALSE = Disable in inspector 
+    public bool HideInInspector = false;
+
+    public EnumHideAttribute(string enumSourceField, int enumOption)
+    {
+        this.EnumSourceField = enumSourceField;
+        this.EnumOption = enumOption;
+        this.HideInInspector = false;
+    }
+
+    public EnumHideAttribute(string enumSourceField, int enumOption, bool hideInInspector)
+    {
+        this.EnumSourceField = enumSourceField;
+        this.EnumOption = enumOption;
+        this.HideInInspector = hideInInspector;
+    }
+}
